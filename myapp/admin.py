4c4a00de1bbaed366960bba91783169fc33713a6
sema_04 <syimykiliasov097@gmail.com>
2025-11-15 13:07:04 +0600

    hw5

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Post, Category, Tag, Comment
 
 
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('image_tag', 'title', 'category', 'rate', 'created_at', 'updated_at')
